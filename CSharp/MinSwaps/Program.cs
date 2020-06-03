e1b68d6fa56f91238e54d3446abc1ea1e6200aa0
MineEnriquez <38847090+MineEnriquez@users.noreply.github.com>
2020-06-02 18:39:06 -0700

    Minimum swaps algorithm
    Minimum swaps algorithm

---
+++ CSharp/MinSwaps/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace MinSwaps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(minimumSwaps(new int[]{7, 1, 3, 2, 4, 5, 6}));
+            Console.WriteLine(minimumSwaps(new int[]{2, 3, 4, 1, 5}));
+            Console.WriteLine(minimumSwaps(new int[]{1, 3, 5, 2, 4, 6, 7}));
+        }
+        
+        static int minimumSwaps(int[] arr) {
+            int _minSwaps=0;
+            for (int i=0; i< arr.Length; i++){
+                Console.WriteLine($"Element {i}:");
+                    while (arr[i]!= i+1) {
+                        int x = arr[i] - 1;
+                        int temp = arr[x];
+                        Console.WriteLine($"Swapping: {arr[i]} for  {arr[x]}  ");
+                        for(int m=0; m<arr.Length ; m++){
+                            Console.Write($"{arr[m]} ");
+                        }
+                        Console.WriteLine();
+                        arr[x] = arr[i];
+                        arr[i] = temp;
+                        for(int m=0; m<arr.Length ; m++){
+                            Console.Write($"{arr[m]} ");
+                        }
+                        _minSwaps += 1;
+                        Console.WriteLine("\n--------------");
+                    }
+            }
+            return _minSwaps;
+        }
+    }
+}
